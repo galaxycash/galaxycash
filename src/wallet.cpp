5dbf863d39ce399b4c5df1a5cde49c5d2eec6c11
galaxycash <galaxycashru@gmail.com>
2018-03-23 07:03:37 +0300

    staking on on default

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2491,7 +2491,6 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     int64_t blockValue = nCredit;
     int64_t masternodePayment = GetMNProofOfStakeReward(nReward, pindexPrev->nHeight + 1);
 
-
     // Set output amount
     if (txNew.vout.size() == 3)
     {
