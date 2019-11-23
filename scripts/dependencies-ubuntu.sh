50b36f2f0fbba74a2986fd6342848503b76c7a1d
GalaxyCash <galaxycashen@gmail.com>
2019-11-23 06:43:11 +0300

    v4.0.0

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -qq
 sudo apt-get upgrade -y -qq
-sudo apt-get install -y -qq autoconf build-essential pkg-config libssl-dev libboost-all-dev
+sudo apt-get install -y -qq devscripts debhelper scratch autoconf build-essential libtool autotools-dev automake pkg-config bsdmainutils curl ca-certificates ccache python3 rsync git libssl-dev libboost-all-dev
 sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext libevent-dev protobuf-compiler libprotobuf-dev
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
