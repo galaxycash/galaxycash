7100f91f7f0900edaf071ac1a333d900d0d111bd
GalaxyCash <galaxycashen@gmail.com>
2019-11-23 06:47:39 +0300

    v4.0.0

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y -qq devscripts debhelper scratch autoconf build-essentia
 sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext libevent-dev protobuf-compiler libprotobuf-dev
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
-sudo apt-get install -y -qq libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y -qq libdb-dev libdb++-dev
 #for debuild
 sudo apt-get install -y -qq libfakeroot fakeroot scratch
