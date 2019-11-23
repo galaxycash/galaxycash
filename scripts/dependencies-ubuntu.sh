b0681b29fbcecbde92882f9a1846bd94ed6ea7bc
GalaxyCash <galaxycashen@gmail.com>
2019-11-23 06:31:24 +0300

    v4.0.0

--- scripts/dependencies-ubuntu.sh
+++ scripts/dependencies-ubuntu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ev
-
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update -qq
 sudo apt-get upgrade -y -qq
 sudo apt-get install -y -qq autoconf build-essential pkg-config libssl-dev libboost-all-dev
 sudo apt-get install -y -qq miniupnpc libminiupnpc-dev gettext libevent-dev protobuf-compiler libprotobuf-dev
 #for gui
 sudo apt-get install -y -qq qtbase5-dev qttools5-dev-tools
-sudo apt-get install -y -qq libdb++-dev
+sudo apt-get install -y -qq libdb4.8-dev libdb4.8++-dev
 #for debuild
 sudo apt-get install -y -qq libfakeroot fakeroot scratch
